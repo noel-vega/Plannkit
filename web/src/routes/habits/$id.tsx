c048c18864a8630854494169798778c9830c1c62
Noel Vega <noelvegajr94@gmail.com>
2026-01-05 17:04:11 -0500

    habit settings dialog

--- web/src/routes/habits/$id.tsx
+++ web/src/routes/habits/$id.tsx
@@ -1,11 +1,13 @@
 import { getHabitByIdQueryOptions } from '@/api'
 import { ContributionsGrid } from '@/components/ContributionsGrid'
 import { Button } from '@/components/ui/button'
+import { Dialog, DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle, DialogTrigger } from '@/components/ui/dialog'
 import { queryClient } from '@/lib/react-query'
 import { useQuery } from '@tanstack/react-query'
 import { createFileRoute, Link } from '@tanstack/react-router'
 import { getDayOfYear } from 'date-fns/getDayOfYear'
-import { MoveLeftIcon } from 'lucide-react'
+import { MoveLeftIcon, SettingsIcon } from 'lucide-react'
+import type { PropsWithChildren } from 'react'
 import z from 'zod/v3'
 
 export const Route = createFileRoute('/habits/$id')({
@@ -29,13 +31,19 @@ function RouteComponent() {
   const contributions = new Map(habit.contributions.map(contrib => [getDayOfYear(contrib.date), contrib]));
   return <div className="max-w-7xl mx-auto">
     <div>
-      <div className="flex gap-3 items-center py-8">
+      <div className="flex gap-8 items-center py-8">
         <Link to="/habits">
           <Button variant="outline" size="icon">
             <MoveLeftIcon />
           </Button>
         </Link>
         <h2 className="text-2xl font-semibold">{habit.name}</h2>
+        <HabitSettingsDialog>
+          <Button variant="secondary" className="border ml-auto">
+            <SettingsIcon />
+            Settings
+          </Button>
+        </HabitSettingsDialog>
       </div>
       <p>{!habit.description ? "No Description" : habit.description}</p>
     </div>
@@ -43,4 +51,30 @@ function RouteComponent() {
       <ContributionsGrid contributions={contributions} />
     </div>
   </div>
+}
+
+
+export function HabitSettingsDialog(props: PropsWithChildren) {
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        {props.children}
+      </DialogTrigger>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>Habit Settings</DialogTitle>
+          <DialogDescription>
+            View and edit your habit settings.
+          </DialogDescription>
+          <div>
+
+          </div>
+          <DialogFooter>
+            <Button>Save</Button>
+          </DialogFooter>
+        </DialogHeader>
+      </DialogContent>
+    </Dialog>
+
+  )
 }
