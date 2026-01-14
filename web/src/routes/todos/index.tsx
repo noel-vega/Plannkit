9177fe25c5e19693a68bc895fd01b91b18bfbfac
Noel Vega <noelvegajr94@gmail.com>
2026-01-14 00:24:16 -0500

    card styles minor

--- web/src/routes/todos/index.tsx
+++ web/src/routes/todos/index.tsx
@@ -1,8 +1,10 @@
+import { Button } from '@/components/ui/button'
 import { Card, CardContent } from '@/components/ui/card'
 import { getListTodosQueryOptions } from '@/features/todos/api'
 import type { Todo } from '@/features/todos/types'
 import { useQuery } from '@tanstack/react-query'
 import { createFileRoute } from '@tanstack/react-router'
+import { PlusIcon } from 'lucide-react'
 
 export const Route = createFileRoute('/todos/')({
   component: RouteComponent,
@@ -33,17 +35,24 @@ function RouteComponent() {
 
 function Lane(props: { title: string, todos: Todo[] }) {
   return (
-    <div className="w-96 border bg-accent rounded">
+    <div className="w-96 border bg-gray-50 rounded">
       <div className="p-4 uppercase text-xs">{props.title}</div>
 
-      <div className="px-1.5 pb-1.5">
-        {props.todos.map(x => (
-          <Card className="py-0 rounded hover:cursor-grab">
-            <CardContent className="p-4">
-              <div className="text-sm">{x.name}</div>
-            </CardContent>
-          </Card>
-        ))}
+      <div className="px-1.5 pb-1.5 space-y-1">
+        <ul>
+          {props.todos.map(x => (
+            <li key={x.id}>
+              <Card className="py-0 rounded hover:cursor-grab hover:bg-neutral-200 hover:border shadow-none">
+                <CardContent className="p-4">
+                  <div className="text-sm">{x.name}</div>
+                </CardContent>
+              </Card>
+            </li>
+          ))}
+        </ul>
+        <div>
+          <Button variant="ghost" className="w-full justify-start hover:bg-neutral-200" ><PlusIcon /> <span>Create</span></Button>
+        </div>
       </div>
     </div>
   )
