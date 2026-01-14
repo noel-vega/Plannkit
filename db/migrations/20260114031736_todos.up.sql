c6f3c3b862697c5a2565ffbaa6bfe8199e025e98
Noel Vega <noelvegajr94@gmail.com>
2026-01-13 22:44:54 -0500

    init todos migrations

---
+++ db/migrations/20260114031736_todos.up.sql
@@ -0,0 +1,10 @@
+
+CREATE TABLE IF NOT EXISTS todos (
+    id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    status VARCHAR(20) NOT NULL DEFAULT 'todo',
+    description TEXT,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT chk_task_status CHECK (status IN ('todo', 'in-progress', 'done'))
+);
