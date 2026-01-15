3f630ea2004ead104f5a0cd9edeade71240c3bdb
Noel Vega <noelvegajr94@gmail.com>
2026-01-14 23:02:47 -0500

    todo card quick options

--- web/src/routes/todos/index.tsx
+++ web/src/routes/todos/index.tsx
@@ -1,11 +1,12 @@
 import { Button } from '@/components/ui/button'
-import { Card, CardContent } from '@/components/ui/card'
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from '@/components/ui/dropdown-menu'
 import { getListTodosQueryOptions } from '@/features/todos/api'
 import { CreateTodoDialog } from '@/features/todos/components/create-todo-dialog'
 import type { Todo, TodoStatus } from '@/features/todos/types'
 import { useQuery } from '@tanstack/react-query'
 import { createFileRoute } from '@tanstack/react-router'
-import { PlusIcon } from 'lucide-react'
+import { EllipsisIcon, PlusIcon } from 'lucide-react'
 
 export const Route = createFileRoute('/todos/')({
   component: RouteComponent,
@@ -34,9 +35,24 @@ function Lane(props: { title: string, status: TodoStatus, todos: Todo[] }) {
         <ul className="space-y-1">
           {props.todos.map(x => (
             <li key={x.id}>
-              <Card className="py-0 rounded hover:cursor-grab hover:bg-neutral-100 hover:border shadow-none">
-                <CardContent className="p-4">
-                  <div className="text-sm">{x.name}</div>
+              <Card className="rounded hover:cursor-grab hover:bg-neutral-100 hover:border shadow-none p-4 group">
+                <CardHeader className="p-0">
+                  <CardTitle className="flex w-full font-normal">
+                    <p className="flex-1">{x.name}</p>
+                    {/*Quick Todo Card Options */}
+                    <div className="w-10">
+                      <DropdownMenu>
+                        <DropdownMenuTrigger asChild>
+                          <Button variant="ghost" size="icon-sm" className="hover:bg-neutral-200 rounded invisible group-hover:visible transition-none"><EllipsisIcon /></Button>
+                        </DropdownMenuTrigger>
+                        <DropdownMenuContent align="end" className="min-w-40">
+                          <DropdownMenuItem>Delete</DropdownMenuItem>
+                        </DropdownMenuContent>
+                      </DropdownMenu>
+                    </div>
+                  </CardTitle>
+                </CardHeader>
+                <CardContent >
                 </CardContent>
               </Card>
             </li>
