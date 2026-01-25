5fff3e3be719fac258182a045644e45b264501ad
Noel Vega <noelvegajr94@gmail.com>
2026-01-25 18:27:32 -0500

    user no password model

--- api/internal/users/users.go
+++ api/internal/users/users.go
@@ -12,3 +12,12 @@ type User struct {
 	CreatedAt time.Time `json:"createdAt" db:"created_at"`
 	UpdatedAt time.Time `json:"updatedAt" db:"updated_at"`
 }
+
+type UserNoPassword struct {
+	ID        int       `json:"id" db:"id"`
+	FirstName string    `json:"firstName" db:"first_name"`
+	LastName  string    `json:"lastName" db:"last_name"`
+	Email     string    `json:"email" db:"email"`
+	CreatedAt time.Time `json:"createdAt" db:"created_at"`
+	UpdatedAt time.Time `json:"updatedAt" db:"updated_at"`
+}
