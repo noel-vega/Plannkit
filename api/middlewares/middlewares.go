a7f0df8a363ad5f18c9716194390fbbe594fc737
Noel Vega <noelvegajr94@gmail.com>
2026-01-26 21:47:14 -0500

    validate jwt and parse claims

--- api/middlewares/middlewares.go
+++ api/middlewares/middlewares.go
@@ -2,11 +2,37 @@
 package middlewares
 
 import (
-	"fmt"
+	"net/http"
+	"strings"
 
 	"github.com/gin-gonic/gin"
+	"github.com/golang-jwt/jwt/v5"
 )
 
+type Claims struct {
+	UserID int `json:"user_id"`
+	jwt.RegisteredClaims
+}
+
 func Guard(c *gin.Context) {
-	fmt.Println("GUARD")
+	authHeader := c.GetHeader("Authorization")
+
+	tokenString := strings.TrimPrefix(authHeader, "Bearer ")
+
+	token, err := jwt.ParseWithClaims(tokenString, &Claims{}, func(token *jwt.Token) (any, error) {
+		return []byte("secret"), nil
+	})
+	if err != nil {
+		c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": "invalid token: " + err.Error()})
+		return
+	}
+
+	claims, ok := token.Claims.(*Claims)
+	if !ok || !token.Valid {
+		c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": "invalid token claims"})
+		return
+	}
+
+	c.Set("user_id", claims.UserID)
+	c.Next()
 }
