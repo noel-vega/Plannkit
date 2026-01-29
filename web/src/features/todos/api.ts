acf03d65e98f050612f01981847dfe58bef5990f
Noel Vega <noelvegajr94@gmail.com>
2026-01-29 18:53:38 -0500

    add headers to todo requests

--- web/src/features/todos/api.ts
+++ web/src/features/todos/api.ts
@@ -2,12 +2,13 @@ import { queryOptions } from "@tanstack/react-query"
 import { TodoSchema, TodoStatusSchema, type CreateTodo, type TodoStatus } from "./types"
 import { queryClient } from "@/lib/react-query"
 import z from "zod/v3"
-
+import { getHeaders } from "@/lib/utils"
 
 const BoardSchema = z.record(TodoStatusSchema, TodoSchema.array())
 
 export async function getBoard() {
-  const response = await fetch("/api/todos/board")
+  const headers = getHeaders()
+  const response = await fetch("/api/todos/board", { headers })
   const json = await response.json()
   return BoardSchema.parse(json)
 }
@@ -24,14 +25,15 @@ export function invalidateGetBoardQuery() {
 }
 
 export async function getTodoById(id: number) {
-  const res = await fetch(`/api/todos/${id}`)
+  const headers = getHeaders()
+  const res = await fetch(`/api/todos/${id}`, { headers })
   const json = await res.json()
   return TodoSchema.parse(json)
 }
 
 export async function listTodos() {
-  console.log("list todos")
-  const res = await fetch("/api/todos")
+  const headers = getHeaders()
+  const res = await fetch("/api/todos", { headers })
   const json = await res.json()
   return TodoSchema.array().parse(json)
 }
@@ -48,18 +50,19 @@ export async function invalidateListTodosQuery() {
 }
 
 export async function createTodo(params: CreateTodo) {
+  const headers = getHeaders()
   await fetch("/api/todos", {
     method: "POST",
     body: JSON.stringify(params),
-    headers: {
-      "content-type": "application/json"
-    }
+    headers
   })
 }
 
 export async function deleteTodo(params: { id: number }) {
+  const headers = getHeaders()
   await fetch(`/api/todos/${params.id}`, {
     method: "DELETE",
+    headers
   })
 }
 
@@ -71,14 +74,12 @@ type MoveTodoParams = {
   targetIndex?: number; // Optional - used for optimistic update only
 }
 export async function moveTodo(params: MoveTodoParams) {
+  const headers = getHeaders()
   const { id, targetIndex, ...rest } = params
-  console.log("MOVE TODO:", params)
   await fetch(`/api/todos/${id}/position`, {
     method: "PATCH",
     body: JSON.stringify(rest), // Don't send targetIndex to server
-    headers: {
-      "content-type": "application/json"
-    }
+    headers
   })
 
 }
