81b05cb57b5dc072adc88a72516555e3651f897f
Noel Vega <noelvegajr94@gmail.com>
2026-02-12 20:52:11 -0500

    refactor headers

--- web/src/lib/plannkit-api-client.ts
+++ web/src/lib/plannkit-api-client.ts
@@ -1,15 +1,17 @@
 import { useAuth } from "@/features/auth/store"
 
-export async function pkFetch(path: string, options?: RequestInit) {
+export async function pkFetch(path: string, options?: RequestInit, json: boolean = true) {
   const { accessToken } = useAuth.getState()
   const url = new URL(path, import.meta.env.VITE_PLANNKIT_API_URL)
+  let headers = {
+    "Authorization": `Bearer ${accessToken}`,
+    ...options?.headers,
+  }
+
+  console.log("HEADERS", headers)
   return await fetch(url, {
     ...options,
     credentials: "include",
-    headers: {
-      ...options?.headers,
-      "Content-Type": "application/json",
-      "Authorization": `Bearer ${accessToken}`,
-    },
+    headers,
   })
 }
