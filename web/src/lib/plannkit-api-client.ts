b10ecea40ccb08677768f4c823d099a76af60334
Noel Vega <noelvegajr94@gmail.com>
2026-02-13 19:23:06 -0500

    handle no json content-type

--- web/src/lib/plannkit-api-client.ts
+++ web/src/lib/plannkit-api-client.ts
@@ -3,9 +3,13 @@ import { useAuth } from "@/features/auth/store"
 export async function pkFetch(path: string, options?: RequestInit, json: boolean = true) {
   const { accessToken } = useAuth.getState()
   const url = new URL(path, import.meta.env.VITE_PLANNKIT_API_URL)
-  let headers = {
+  let headers = new Headers({
     "Authorization": `Bearer ${accessToken}`,
-    ...options?.headers,
+    ...options?.headers
+  })
+
+  if (json) {
+    headers.set("Content-Type", "application/json")
   }
 
   console.log("HEADERS", headers)
