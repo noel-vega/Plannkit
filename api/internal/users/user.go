7909f9e975cf04a88bdd668f6c13beafab22bad4
Noel Vega <noelvegajr94@gmail.com>
2026-02-13 19:24:32 -0500

    add avatar property

--- api/internal/users/user.go
+++ api/internal/users/user.go
@@ -9,6 +9,7 @@ type User struct {
 	LastName  string    `json:"lastName" db:"last_name"`
 	Password  string    `json:"password" db:"password"`
 	Email     string    `json:"email" db:"email"`
+	Avatar    string    `json:"avatar" db:"avatar"`
 	CreatedAt time.Time `json:"createdAt" db:"created_at"`
 	UpdatedAt time.Time `json:"updatedAt" db:"updated_at"`
 }
@@ -18,6 +19,7 @@ type UserNoPassword struct {
 	FirstName string    `json:"firstName" db:"first_name"`
 	LastName  string    `json:"lastName" db:"last_name"`
 	Email     string    `json:"email" db:"email"`
+	Avatar    string    `json:"avatar" db:"avatar"`
 	CreatedAt time.Time `json:"createdAt" db:"created_at"`
 	UpdatedAt time.Time `json:"updatedAt" db:"updated_at"`
 }
