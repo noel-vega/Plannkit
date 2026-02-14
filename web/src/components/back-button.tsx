30dfd879cdfee3dee90d72e2ef52ba3912ac5d96
Noel Vega <noelvegajr94@gmail.com>
2026-02-13 22:06:23 -0500

    can go back and optional to

--- web/src/components/back-button.tsx
+++ web/src/components/back-button.tsx
@@ -1,14 +1,28 @@
-import { Link } from "@tanstack/react-router";
+import { useCanGoBack, useRouter } from "@tanstack/react-router";
 import { Button } from "./ui/button";
 import { MoveLeftIcon } from "lucide-react";
 
-export function BackButton({ to }: { to: string }) {
-  return (
+export function BackButton({ to }: { to?: string }) {
+  const canGoBack = useCanGoBack()
+  const router = useRouter()
+  const handleClick = () => {
+    if (to) {
+      router.navigate({ to })
+      return
+    }
 
-    <Link to={to}>
-      <Button variant="outline" size="icon">
-        <MoveLeftIcon />
-      </Button>
-    </Link>
+    if (canGoBack) {
+      router.history.back()
+    } else {
+      router.navigate({ to: "/app/dashboard" })
+    }
+
+
+  }
+
+  return (
+    <Button variant="outline" size="icon" onClick={handleClick}>
+      <MoveLeftIcon />
+    </Button>
   )
 }
