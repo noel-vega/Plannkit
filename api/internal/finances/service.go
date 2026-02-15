da176c407b7703ab996980a4e9f088a4b54e854a
Noel Vega <noelvegajr94@gmail.com>
2026-02-14 21:46:06 -0500

    service

--- api/internal/finances/service.go
+++ api/internal/finances/service.go
@@ -1,1 +1,13 @@
 package finances
+
+import "github.com/jmoiron/sqlx"
+
+type Service struct {
+	repository *Repository
+}
+
+func NewService(db *sqlx.DB) *Service {
+	return &Service{
+		repository: NewRepository(db),
+	}
+}
