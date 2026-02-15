6a98272371b590307a40568921f49c204607902d
Noel Vega <noelvegajr94@gmail.com>
2026-02-15 01:45:37 -0500

    use env var when parsing avatar

--- web/src/features/auth/types.ts
+++ web/src/features/auth/types.ts
@@ -2,7 +2,7 @@ import z from "zod/v3"
 
 
 export const AvatarSchema = z.string().nullable().transform(filename => {
-  return filename ? `http://localhost:8080/public/avatars/${filename}` : null
+  return filename ? `${import.meta.env.VITE_PLANNKIT_API_URL}/public/avatars/${filename}` : null
 })
 
 export const MeSchema = z.object({
