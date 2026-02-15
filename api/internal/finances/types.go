0d811e160e1bd1bd355768e7f8ca6574246f46e4
Noel Vega <noelvegajr94@gmail.com>
2026-02-14 21:45:55 -0500

    finance space type

--- api/internal/finances/types.go
+++ api/internal/finances/types.go
@@ -1,4 +1,14 @@
 // Package finances
 package finances
 
+import "time"
+
+type Space struct {
+	ID        int       `json:"id" db:"id"`
+	UserID    int       `json:"userId" db:"user_id"`
+	Name      string    `json:"name" db:"name"`
+	CreatedAt time.Time `json:"createdAt" db:"created_at"`
+	UpdatedAt time.Time `json:"updatedAt" db:"updated_at"`
+}
+
 type Goal struct{}
