25d1c0950b70a03687d5addb3146ed163c22a225
Noel Vega <noelvegajr94@gmail.com>
2026-02-19 00:00:22 -0500

    goal type

--- api/internal/finances/types.go
+++ api/internal/finances/types.go
@@ -40,7 +40,27 @@ type CreateExpenseParams struct {
 	Description *string `json:"description" db:"description"`
 }
 
-type Goal struct{}
+// CREATE TABLE IF NOT EXISTS finance_spaces_goals (
+//   id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+//   finance_space_id INT NOT NULL REFERENCES finance_spaces(id) ON DELETE CASCADE,
+//   user_id INT NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+//   name TEXT NOT NULL,
+//   amount INT NOT NULL,
+//   monthly_commitment INT NOT NULL,
+//   created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+//   updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+// );
+
+type Goal struct {
+	ID                int       `json:"id" db:"id"`
+	SpaceID           int       `json:"spaceId" db:"finance_space_id"`
+	UserID            int       `json:"userId" db:"user_id"`
+	Name              string    `json:"name" db:"name"`
+	Amount            int       `json:"amount" db:"amount"`
+	MonthlyCommitment int       `json:"monthlyCommitment" db:"monthly_commitment"`
+	CreatedAt         time.Time `json:"createdAt" db:"created_at"`
+	UpdatedAt         time.Time `json:"updatedAt" db:"updated_at"`
+}
 
 type DeleteExpenseParams struct {
 	ID      int `json:"id" db:"id"`
