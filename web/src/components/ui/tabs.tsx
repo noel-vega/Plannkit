5688159e07695d3e1dedd9315f87c48201569a73
Noel Vega <noelvegajr94@gmail.com>
2026-02-22 18:30:54 -0500

    add border to tab list default styles

--- web/src/components/ui/tabs.tsx
+++ web/src/components/ui/tabs.tsx
@@ -28,7 +28,7 @@ const tabsListVariants = cva(
   {
     variants: {
       variant: {
-        default: "bg-muted",
+        default: "bg-muted border",
         line: "gap-1 bg-transparent",
       },
     },
