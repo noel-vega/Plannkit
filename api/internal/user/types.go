9853543399a81b73a441b134fcf428a5f44c383a
Noel Vega <noelvegajr94@gmail.com>
2026-02-26 22:33:46 -0500

    user IsPrivate field

--- api/internal/user/types.go
+++ api/internal/user/types.go
@@ -10,6 +10,7 @@ type User struct {
 	LastName  string    `json:"lastName" db:"last_name"`
 	Password  string    `json:"password" db:"password"`
 	Email     string    `json:"email" db:"email"`
+	IsPrivate bool      `json:"isPrivate" db:"is_private"`
 	Avatar    *string   `json:"avatar" db:"avatar"`
 	CreatedAt time.Time `json:"createdAt" db:"created_at"`
 	UpdatedAt time.Time `json:"updatedAt" db:"updated_at"`
@@ -21,6 +22,7 @@ type UserNoPassword struct {
 	FirstName string    `json:"firstName" db:"first_name"`
 	LastName  string    `json:"lastName" db:"last_name"`
 	Email     string    `json:"email" db:"email"`
+	IsPrivate bool      `json:"isPrivate" db:"is_private"`
 	Avatar    *string   `json:"avatar" db:"avatar"`
 	CreatedAt time.Time `json:"createdAt" db:"created_at"`
 	UpdatedAt time.Time `json:"updatedAt" db:"updated_at"`
