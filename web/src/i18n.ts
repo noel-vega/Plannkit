79cbc39712df223e93fd65e03eabc7c4bf7b3795
Noel Vega <noelvegajr94@gmail.com>
2026-03-18 23:43:01 -0400

    todo: persist language selection

--- web/src/i18n.ts
+++ web/src/i18n.ts
@@ -4,6 +4,8 @@ import { initReactI18next } from "react-i18next";
 // the translations
 // (tip move them in a JSON file and import them,
 // or even better, manage them separated from your code: https://react.i18next.com/guides/multiple-translation-files)
+//
+// TODO: persist language selection
 const resources = {
   en: {
     translation: {
