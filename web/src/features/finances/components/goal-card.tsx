53ccc842cf3ca5e0428407c9303f70314ea13a1c
Noel Vega <noelvegajr94@gmail.com>
2026-04-08 23:05:02 -0400

    fix nested button

--- web/src/features/finances/components/goal-card.tsx
+++ web/src/features/finances/components/goal-card.tsx
@@ -64,7 +64,7 @@ export function GoalCard({ goal, onAction }: GoalCardProps) {
               </div>
             </div>
             <DropdownMenu>
-              <DropdownMenuTrigger>
+              <DropdownMenuTrigger asChild>
                 <Button variant="ghost">
                   <MoreVerticalIcon />
                 </Button>
